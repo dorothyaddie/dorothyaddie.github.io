4283d2811d205e38b6f0d46577223c77c8b89db1
Dorothy Addie <dorothy.grace.a@gmail.com>
2025-01-27 00:30:54 -0500

    about page w fun scroll

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
+// makes nav bar links stay red after click
 document.addEventListener('DOMContentLoaded', () => {
-    // Select all links with the 'nav_link' class
     const navLinks = document.querySelectorAll('.nav_link');
   
     navLinks.forEach(link => {
-      // Add click event to each link
       link.addEventListener('click', () => {
-        // Remove 'active' class from all links
         navLinks.forEach(link => link.classList.remove('active'));
   
-        // Add 'active' class to the clicked link
         link.classList.add('active');
       });
     });
   });
+
+// Makes flowers on the right scroll down to about section
+document.addEventListener('scroll', () => {
+const scrollY = window.scrollY; 
+const image = document.querySelector('.flowers_right');
+
+const viewportHeight = window.innerHeight;
+const maxScroll = 1 * viewportHeight;
+const limitedScroll = Math.min(scrollY, maxScroll);
+
+image.style.transform = `translate(0%, calc(0% + ${limitedScroll}px))`
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.remove("hidden"); 
+        }
+      });
+    });
+  
+    const sections = document.querySelectorAll(".content");
+    sections.forEach((section) => observer.observe(section));
+  });
+  
