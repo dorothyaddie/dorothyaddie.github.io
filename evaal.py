5af01072c36babd57bf4d013b7ac78d322bddcc8
Dorothy "Addie" Addie <74277916+dorothyaddie@users.noreply.github.com>
2025-02-06 16:18:24 -0500

    Delete eval.py

--- evaal.py
+++
@@ -1,6 +0,0 @@
-precision = np.cumsum(tp) / (np.cumsum(tp) + np.cumsum(fp))
-recall = np.cumsum(tp) / num_gt_boxes
-# make this curve monotonically decreasing
-
-ap = np.trapz(precision, recall)
-return ap
